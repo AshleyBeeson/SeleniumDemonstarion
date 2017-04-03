3eb1d9fc5a813e1679dd0faea94425a2c92cf0bd
Ashley.Beeson <ashley.beeson@qa.com>
2017-04-03 11:39:59 +0100

    setup commit

---
+++ Demo/src/test/java/seleniumdemo/SeleniumTest.java
@@ -0,0 +1,26 @@
+package seleniumdemo;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SeleniumTest {
+
+    @Before
+    public void setUp(){
+
+    }
+
+    @Test
+    public void test(){
+
+    }
+
+    @After
+    public void tearDown(){
+
+    }
+
+
+}
