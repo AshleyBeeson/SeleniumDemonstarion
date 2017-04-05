57a0a0720c8ca9cb0b38201cad83799c1c87a7b9
Ashley.Beeson <ashley.beeson@qa.com>
2017-04-05 22:43:03 +0100

    Initial commit

--- Cinema/src/main/java/PriceCalculator.java
+++ Cinema/src/main/java/PriceCalculator.java
@@ -8,11 +8,12 @@ public class PriceCalculator{
 
     public static String calculate(List<Ticket> selectedTickets){
         System.out.println("CALCULATING...");
-        isWednesday = "Wed".equalsIgnoreCase(new SimpleDateFormat("ddd").format(new Date()));
+        isWednesday = "Wed".equalsIgnoreCase(new SimpleDateFormat("EEE").format(new Date()));
         int totalCost = 0;
         for (Ticket ticket : selectedTickets){
             int cost = ticket.getCost();
             if (isWednesday){
+                System.out.println("WEDNESDAY DISCOUNT");
                 cost -= 2;
             }
             totalCost += cost;
